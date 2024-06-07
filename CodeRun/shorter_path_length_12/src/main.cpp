118623814eb78d16e5ef4fc3d7feeeb0f107e472
m0r15 <serenkovav@gmail.com>
2024-06-07 09:07:11 +0300

    added shorter path length

--- CodeRun/shorter_path_length_12/src/main.cpp
+++ CodeRun/shorter_path_length_12/src/main.cpp
@@ -1,5 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
 
 int main(int argc, char *argv[])
 {
+    // Input
+    int N = 0;
+    std::cin >> N;
+
+    std::vector<std::vector<int>> matrix(N);
+    for (size_t i = 0; i < N; ++i) {
+        matrix[i] = std::vector<int>(N);
+        std::string item;
+        size_t j = 0;
+        while (j < N && std::getline(std::cin, item, ' ')) {
+            if (!item.empty()) {
+                matrix[i][j] = std::stoi(item);
+            }
+            ++j;
+        }
+    }
+
+    // vertex
+    int v_begin, v_end;
+    std::cin >> v_begin >> v_end;
+
     return 0;
 }
